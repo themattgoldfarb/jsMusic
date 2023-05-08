6e8acc2636463a38ec655d5443c60fa7d39dd53f
Matt Goldfarb <themattgoldfarb@gmail.com>
2023-05-08 17:32:01 -0400

    Basic music sequencer.

--- js-music/src/player.tsx
+++ js-music/src/player.tsx
@@ -1,28 +1,119 @@
-import React, { useState, useRef } from "react";
-import Sound, { NoteValue, PlayingState } from "./sound";
+import React, { useState, useRef, useEffect } from "react";
+import Sound, { NoteValue, PlayingState, noteEquals, NoteLength} from "./sound";
+
+import * as d3 from "d3";
+
+import './player.css'
+
+enum NoteLetter {
+  C = 0, D = 1, E = 2, F = 3, G = 4, A = 5, B = 6
+}
+
+const noteValues = (noteLetter: number) => (octave: number) => {
+  return {
+    name: NoteLetter[noteLetter % 7] + (octave + Math.floor(noteLetter / 7)),
+    frequency: 440 * Math.pow(2, octave - 4 + (noteLetter - 1) / 7),
+  };
+
+}
+
+function TimeGrid({ progress, notes, clickHandler }
+  : { progress: number, notes: NoteValue[], clickHandler: (row: number, col: number) => void }) {
+
+  function color(row: number, col: number) {
+    if (notes.some(note => note.startSixteenth === col && note.name === noteValues(row)(2).name)) {
+      return "green";
+    }
+    return "white";
+  }
 
 
-const NoteValues: NoteValue[] = [
-  { name: "C", frequency: 65.41 },
-  { name: "D", frequency: 73.42 },
-  { name: "E", frequency: 82.41 },
-  { name: "F", frequency: 87.31 },
-  { name: "G", frequency: 98.00 },
-  { name: "A", frequency: 110.00 },
-  { name: "B", frequency: 123.47 },
-];
+  const ROWS=32;
+  const COLS=16;
+  return (
+    <div>
+      <svg className="time-grid-draw" width="100%" height="100%">
+        <g>
+          {d3.range(0, COLS, 1).map(col =>
+            d3.range(0, ROWS, 1).map(row =>
+              <>
+                <rect
+                  width={100/COLS+"%"} height={100/ROWS+"%"}
+                  x={col * 100/COLS + "%"} y={row * 100/ROWS + "%"}
+                  fill={color(row,col)} stroke="black" strokeWidth="1"
+                  onClick={clickHandler.bind(null, row, col)}
+                >
+                </rect>
+                <text
+                  x={(col * 100 + 50)/COLS + "%"} y={(row * 100 + 50)/ROWS + "%"}
+                  fill="black" fontSize="12" textAnchor="middle" alignmentBaseline="middle"
+                  onClick={clickHandler.bind(null, row, col)}
+                >
+                  {noteValues(row)(2).name + "-" + col}
+                </text>
+              </>
+            )
+          )}
+          <line className="time-line" key={"x-time" + progress} x1={progress + "%"} y1="0%" x2={progress + "%"} y2="100%" stroke="red" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
 
 
 function Player() {
+
   const [playingState, setPlayingState] = useState(PlayingState.Playing);
   const [notes, setNotes] = useState<NoteValue[]>([]);
+  const [progress, setProgress] = useState(0);
+  const [beat, setBeat] = useState(0);
+
+  const bpm = 80;
+
+  function tickAnimation(elapsed: number) {
+    const msInMinute = 60000;
+    const msPerBeat = msInMinute / bpm;
+    const msPerScreen = msPerBeat * 4;
+
+    setProgress(elapsed % msPerScreen / msPerScreen * 100);
+    setBeat(Math.floor(elapsed % msPerScreen / msPerScreen * 16));
+  }
+
+
+  useEffect(() => {
+    const t = d3.timer(tickAnimation);
+    return () => t.stop();
+  }, []);
+
+  function clickHandler(row: number, col: number) {
+    let note = {
+      ...noteValues(row)(2),
+      duration: NoteLength.Quarter,
+      startSixteenth: col,
+
+    }
+    toggleNote(note);
+  }
 
   function handlePlay() {
     setPlayingState(PlayingState.Playing);
   }
 
+  function toggleNote(note: NoteValue) {
+    if (!notes.find(noteEquals(note))) {
+      setNotes([...notes, note]);
+    }
+    else {
+      setNotes(notes.filter(n => !noteEquals(note)(n)));
+    }
+  }
+
   function handleNote(note: NoteValue) {
-    setNotes([...notes, note]);
+    if (!notes.find(noteEquals(note))) {
+      setNotes([...notes, note]);
+    }
   }
 
   function handlePause() {
@@ -34,40 +125,34 @@ function Player() {
   }
 
   return (
-    <div>
+    <div className="player">
+      <TimeGrid progress={progress} notes={notes} clickHandler={clickHandler} />
       <div>
-        <Sound playingState={playingState} notes={notes} />
-        <div>{playingState === PlayingState.Playing ? "Playing" : "Paused"}</div>
-        <button onClick={handlePlay}>Play</button>
-        <button onClick={handlePause}>Pause</button>
-        <button onClick={handleClear}>Clear</button>
+        {playingState === PlayingState.Playing ? "Playing" : "Paused"}
+        {beat}
+        <Sound playingState={playingState} notes={notes} bpm={bpm} beat={beat}/>
+        <div>
+          <span>
+            <button onClick={handlePlay}>Play</button>
+            <button onClick={handlePause}>Pause</button>
+            <button onClick={handleClear}>Clear</button>
+          </span>
+        </div>
       </div>
-      <table>
-        <tbody>
-          {[2, 3, 4, 5, 6, 7, 8].map(octave =>
-            <tr key={"tr-" + octave}>
-              {NoteValues.map(note =>
-                <td key={"td-" + note.name + octave}>
-                  <NoteButton note={note} octave={octave} onClick={handleNote} />
-                </td>
-              )}
-            </tr>
-          )}
-        </tbody>
-
-      </table>
     </div>
   );
 }
 
 function NoteButton({ note, octave, onClick }
   : { note: NoteValue, octave: number, onClick: (note: NoteValue) => void }) {
-  return <button onClick={() => onClick({
-    frequency: note.frequency * (2 ** (octave - 2)),
-    name: note.name
-  })} >
-    {note.name + octave}
-  </button>
+  return (
+    <div className="note-button" onClick={() => onClick({
+      frequency: note.frequency * (2 ** (octave - 2)),
+      name: note.name + octave
+    })} >
+      {note.name + octave}
+    </div>
+  );
 }
 
 export default Player;
--- js-music/src/sound.tsx
+++ js-music/src/sound.tsx
@@ -1,94 +1,77 @@
-import React, { useState, useRef } from "react";
+import React, { useRef, useEffect } from "react";
 
 export enum PlayingState {
   Playing,
   Paused,
 }
 
+export enum NoteLength {
+  Whole, Half, Quarter, Eighth, Sixteenth,
+}
+
 export interface NoteValue {
   name: string;
   frequency: number;
+  startSixteenth?: number;
+  duration?: NoteLength;
 }
+
+export const noteEquals = (note: NoteValue) => (other: NoteValue) => 
+      Math.floor(note.frequency) == Math.floor(other.frequency)
+      && note.name === other.name
+      && note.startSixteenth === other.startSixteenth
+      && note.duration === other.duration;
+
 
 interface SoundInput {
   playingState: PlayingState;
   notes: NoteValue[];
+  bpm: number;
+  beat: number;
 }
 
-interface Context {
-  audioCtx?: AudioContext;
-  oscillators: OscillatorNode[];
-}
-
-
-
-function Sound({ playingState, notes }: SoundInput) {
-
-  let context = useRef<Context>({ oscillators: [] });
-
-  const compareApprox = (a: number) => (b: number) => a - 1 < b && a + 1 > b;
-
-  const compareOscillator = (o: OscillatorNode) => (n: NoteValue) => {
-    return compareApprox(o.frequency.value)(n.frequency);
-  }
-
-  const compareNote = (n: NoteValue) => (o: OscillatorNode) => {
-    return compareApprox(n.frequency)(o.frequency.value);
-  }
-
-  function getAudioCtx() {
-    if (context.current.audioCtx === undefined) {
-      context.current.audioCtx = new AudioContext();
-    }
-    return context.current.audioCtx;
-  }
-
-  function createOscillator(note: NoteValue) {
-    let oscillator = getAudioCtx()!.createOscillator();
+function SingleNote({ note, audioCtx }
+  : { note: NoteValue, audioCtx: AudioContext }) {
+  useEffect(() => {
+    let oscillator = audioCtx.createOscillator();
+    let gain = audioCtx.createGain();
+    oscillator.connect(gain);
+    gain.connect(audioCtx.destination);
     oscillator.type = "sine";
     oscillator.frequency.value = note.frequency;
-    oscillator.connect(getAudioCtx()!.destination);
     oscillator.start();
-    return oscillator;
+    return () => {
+      gain.gain.exponentialRampToValueAtTime(0.00001, audioCtx.currentTime + .5);
+      oscillator.stop(audioCtx.currentTime + 1);
+
+    }
+  }, [note, audioCtx]);
+  return <span>{note.name}</span>
+
+}
+
+function Sound({ playingState, notes, bpm, beat}: SoundInput) {
+
+  let audioCtx = useRef<AudioContext>();
+
+  function getAudioCtx() {
+    if (audioCtx.current === undefined) {
+      audioCtx.current = new AudioContext();
+    }
+    return audioCtx.current;
   }
 
-  function handlePlaying(): void {
-    let newOscillators: OscillatorNode[] = [];
-
-    // Create Oscillators for each missing note missing an oscillator.
-    notes.forEach(note => {
-      if (!context.current.oscillators.some(compareNote(note))) {
-        newOscillators.push(createOscillator(note));
-      }
-    });
-
-    // Add Oscillators for each oscillator already with a note.
-    // Stop any oscillators that don't have a note.
-    context.current.oscillators.forEach(oscillator => {
-      if (!notes.some(compareOscillator(oscillator))) {
-        oscillator.stop();
-      } else {
-        newOscillators.push(oscillator);
-      }
-    });
-
-    context.current.oscillators = newOscillators;
-  }
-
-  function handlePaused(): void {
-    context.current.oscillators.forEach(oscillator => {
-      oscillator.stop();
-    });
-    context.current.oscillators = [];
-  }
 
   switch (playingState) {
-    case PlayingState.Playing:
-      handlePlaying();
-      break;
-    case PlayingState.Paused:
-      handlePaused();
-      break;
+    case PlayingState.Playing: {
+      return (
+        <span>
+          {notes.filter(note => note.startSixteenth === beat).map(note =>
+            <SingleNote key={"singlenote-"+note.name } note={note} audioCtx={getAudioCtx()!} />
+          )}
+        </span>
+      )
+    }
     default:
       break;
   }
